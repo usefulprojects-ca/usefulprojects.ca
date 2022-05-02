dfdd543daca537444cd675797cde3172e4828361
Robert Warner <robertfrederickwarner@gmail.com>
2022-05-01 23:07:22 -0700

    Tweak config code to also override pprint behaviour

--- src/ca/usefulprojects/config.clj
+++ src/ca/usefulprojects/config.clj
@@ -1,20 +1,23 @@
 (ns ca.usefulprojects.config
   (:require
    [aero.core :as aero]
+   [clojure.pprint :as pprint]
    [clojure.walk :as walk]))
 
 (defmethod aero/reader 'deep-merge
   [_opts _tag values]
   (apply merge-with (cons merge values)))
 
+;; a record is used primarily for simple overriding of print/pprint behaviour
+(defrecord Config [secrets overrides])
+
 (defn read-config [profile]
-  ;; setting the :type meta allows for overriding print behaviour
-  (with-meta (aero/read-config "config/config.edn" {:profile profile})
-    {:type ::config-map}))
+  (map->Config (aero/read-config "config/config.edn" {:profile profile})))
 
 (comment
   (read-config :local)
-  (type (read-config :local)))
+  (type (read-config :local))
+  (class (read-config :local)))
 
 (defn- tree-leaves [t]
   (let [branch? #(or (sequential? %) (map? %))]
@@ -24,18 +27,21 @@
   (tree-leaves (read-config :local)))
 
 (defn sanitize
-  "Censors any secrets in config to make it suitable for printing."
+  "Censors any secrets in config to make it suitable for printing.
+  Returns a Map instead of a Config record."
   [config]
   (let [secrets (-> (:secrets config) tree-leaves set)]
-    (walk/postwalk #(if (contains? secrets %) "*****" %) config)))
+    (walk/postwalk #(if (contains? secrets %) "*****" %) (into {} config))))
 
 (comment
   (sanitize (read-config :local)))
 
-(defmethod print-method ::config-map [x ^java.io.Writer w]
-  (.write w (print-str (-> x sanitize (vary-meta dissoc :type)))))
+(defmethod print-method Config [x ^java.io.Writer w]
+  (.write w (print-str (sanitize x))))
+
+(defmethod pprint/simple-dispatch Config [x]
+  (pprint/pprint (sanitize x)))
 
 (comment
-  ;; TODO: figure out how to extend pprint to also have sanitize behaviour
-  (require '[clojure.pprint :as pprint])
+  (with-out-str (print (read-config :local)))
   (with-out-str (pprint/pprint (read-config :local))))
